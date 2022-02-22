f8d7434fc40a52aebbb677fc9045990cdb4c798b
Dmitrijs <ggamersshd@gmail.com>
2022-02-22 15:23:35 +0200

    Pabeigts projekts

---
+++ Assets/Skripti/KursorsUzAttela.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KursorsUzAttela : MonoBehaviour {
+	public AudioSource skanasAvots;
+	public AudioClip skanaKoAtskanot;
+
+	public void uzbiditsUzAttela(){
+		skanasAvots.PlayOneShot (skanaKoAtskanot);
+	}
+	public void nobiditsNoAttela(){
+		skanasAvots.Stop ();
+	}
+
+}
